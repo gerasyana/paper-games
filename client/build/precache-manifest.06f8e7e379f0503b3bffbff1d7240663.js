7a18d45af8b1fd7caa133f49eac25f36a33c068a
yana.gerasimchuk-vrp <yana.gerasimchuk-vrp@bullhorn.com>
2019-03-23 15:29:15 +0300

    Added build

---
+++ client/build/precache-manifest.06f8e7e379f0503b3bffbff1d7240663.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "600335655cfa450fdb98",
+    "url": "/static/js/runtime~main.4ff8a8ce.js"
+  },
+  {
+    "revision": "114aef0c5224737e6de1",
+    "url": "/static/js/main.0cadd0df.chunk.js"
+  },
+  {
+    "revision": "d670d3c1fa6e3a730943",
+    "url": "/static/js/5.dd76fe9c.chunk.js"
+  },
+  {
+    "revision": "a76a84e64a2f7eadd5a3",
+    "url": "/static/js/4.f4174e72.chunk.js"
+  },
+  {
+    "revision": "6d4b3dba939080b1a196",
+    "url": "/static/js/3.2a2258d1.chunk.js"
+  },
+  {
+    "revision": "3208595f93463edf580b",
+    "url": "/static/js/2.9698a296.chunk.js"
+  },
+  {
+    "revision": "114aef0c5224737e6de1",
+    "url": "/static/css/main.e6bb47f9.chunk.css"
+  },
+  {
+    "revision": "fffdbdeccc615aa11ff84e5813fee86e",
+    "url": "/index.html"
+  }
+];
