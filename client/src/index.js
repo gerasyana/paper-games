67ce3decbdc0336b3619199ea54404bb248af669
yana.gerasimchuk-vrp <yana.gerasimchuk-vrp@bullhorn.com>
2019-03-30 22:42:45 +0300

    Fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,30 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import createSagaMiddleware from 'redux-saga'
 
 import App from './App';
-import authReducer from './storage/reducers/auth';
-import roomReducer from './storage/reducers/room';
-import { watchAuth, watchRoom } from './storage/sagas/sagas';
-
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
-const sagaMiddleware = createSagaMiddleware();
-
-const store = createStore(
-    combineReducers({
-        auth: authReducer,
-        room : roomReducer
-    }),
-    composeEnhancers(
-        applyMiddleware(sagaMiddleware)
-    )
-);
-
-sagaMiddleware.run(watchAuth);
-sagaMiddleware.run(watchRoom);
+import store from './storage/store';
 
 const app = (
     <Provider store={store}>
--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -5,15 +5,15 @@ module.exports.authenticate = async (req, res, next) => {
     const token = req.headers[keys.AUTH_HEADER];
 
     if (!token) {
-        return res.json({ error: 'Auth token is not supplied' });
+        return res.status(401);
     }
 
     const isValid = await isValidJWT(token);
-    
+
     if (isValid) {
         req.token = token;
         next();
     } else {
-        return res.json({ error: 'Token is not valid' });
+        return res.status(401);
     }
 }
