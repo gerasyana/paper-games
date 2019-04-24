3e12aa89698d223d40f2eb727f4bcd9280d03eeb
yana.gerasimchuk-vrp <yana.gerasimchuk-vrp@bullhorn.com>
2019-04-24 14:22:03 +0300

    -

--- server/init-scripts/routes.js
+++ server/init-scripts/routes.js
@@ -5,11 +5,11 @@ const path = require('path');
 module.exports = app => {
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../../client/build')));
-        app.get('/*', (req, res) => {
+        app.get('/', (req, res) => {
             res.sendFile(path.join(__dirname, '../../client/build/index.html'));
         });
     } else {
-        app.get('/*', (req, res) => {
+        app.get('/', (req, res) => {
             res.send({ status: 200 });
         })
     }
