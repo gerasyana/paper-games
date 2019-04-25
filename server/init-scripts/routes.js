bceab4b2a372d473ed6137de802b976fccc8a309
yana.gerasimchuk-vrp <yana.gerasimchuk-vrp@bullhorn.com>
2019-04-25 17:59:24 +0300

    Fixed routes

--- server/init-scripts/routes.js
+++ server/init-scripts/routes.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = app => {
+    const fileNames = fs.readdirSync(path.resolve(__dirname, '../routes'));
+    fileNames.forEach(fileName => require(`../routes/${fileName}`)(app));
+
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../../client/build')));
         app.get('/*', (req, res) => {
@@ -13,6 +16,4 @@ module.exports = app => {
             res.send({ status: 200 });
         })
     }
-    const fileNames = fs.readdirSync(path.resolve(__dirname, '../routes'));
-    fileNames.forEach(fileName => require(`../routes/${fileName}`)(app));
 } 
