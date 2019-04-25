8c7993754082d55ac7ff6837d2fda472fda5ac83
yana.gerasimchuk-vrp <yana.gerasimchuk-vrp@bullhorn.com>
2019-04-25 17:39:42 +0300

    Fixed

--- server/init-scripts/routes.js
+++ server/init-scripts/routes.js
@@ -9,7 +9,7 @@ module.exports = app => {
             res.sendFile(path.join(__dirname, '../../client/build/index.html'));
         });
     } else {
-        app.get('/*', (req, res) => {
+        app.get('/', (req, res) => {
             res.send({ status: 200 });
         })
     }
