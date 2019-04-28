42f77a02946d71216cdee597938ccfaa28ac53a3
yana.gerasimchuk-vrp <yana.gerasimchuk-vrp@bullhorn.com>
2019-04-28 19:52:13 +0300

    Remove

--- server/services/socket.js
+++ server/services/socket.js
@@ -11,7 +11,6 @@ class SocketClient {
 
     initConnection() {
         this.io.on('connection', async (client) => {
-            console.log('user connected ', client.id);
             client.on('setUserId', async (data) => {
                 await redis.sockets.save(client.id, data.userId);
                 const ioDetails = await this.getConnectionDetails();
